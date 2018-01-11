3fb89ca8652172a37868869f59bea963f7f41b8e
Casey Watts <casey.s.watts@gmail.com>
2018-01-11 16:45:02 -0500

    try to get initial state working again now that there's a 2nd file

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express()
 const path = require('path')
 
 // state
-let theSharedString = 'hi'
+let state = {theSharedString: 'hi'}
 const PORT = process.env.PORT || 3000
 const INDEX = path.join(__dirname, 'index.html')
 
@@ -18,12 +18,18 @@ wss.on('connection', (ws) => {
   console.log('Client connected')
   ws.on('close', () => console.log('Client disconnected'))
 
+  sendState(ws)
+
   ws.on('message', (data) => {
     console.log('received ' + data)
-    theSharedString = data
+    state.theSharedString = data
     wss.clients.forEach((client) => {
-      console.log('sending ' + theSharedString)
-      client.send(theSharedString)
+      console.log('sending ' + JSON.stringify(state))
+      sendState(client)
     })
   })
 })
+
+const sendState = (destination) => {
+  destination.send(JSON.stringify(state), () => { console.log('error sending state whoopsies') })
+}
