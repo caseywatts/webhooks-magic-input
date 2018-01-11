f11c943d293f609c72982d48cc0a9d3c7911aa32
Casey Watts <casey.s.watts@gmail.com>
2018-01-11 12:34:44 -0500

    magic shared input

--- server.js
+++ server.js
@@ -3,17 +3,26 @@ const ws = require('ws')
 const SocketServer = ws.Server
 const app = express()
 
+// state
+let theSharedString = "hi";
+
 const server = app.get('/', (req, res) => res.send(`
-heya
-<div id='server-time'></div>
+Magic Input
+<input id="theSharedString" value="hi">
 <script>
 var HOST = location.origin.replace(/^http/, 'ws')
 var ws = new WebSocket(HOST);
-var el = document.getElementById('server-time');
+var el = document.getElementById('theSharedString');
 
 ws.onmessage = function (event) {
-  el.innerHTML = 'Server time: ' + event.data;
+  // console.log(event.data);
+  el.value = event.data;
 };
+
+el.oninput = function (event) {
+  // console.log(el.value);
+  ws.send(el.value);
+}
 </script>
   `))
 .listen(3000, () => console.log('Example app listening on port 3000!'))
@@ -21,12 +30,14 @@ ws.onmessage = function (event) {
 const wss = new SocketServer({ server });
 
 wss.on('connection', (ws) => {
-  console.log('Client connected');
+  // console.log('Client connected');
   ws.on('close', () => console.log('Client disconnected'));
-});
 
-setInterval(() => {
-  wss.clients.forEach((client) => {
-    client.send(new Date().toTimeString());
-  });
-}, 1000);
+  ws.on('message', (data) => {
+    // console.log(data);
+    theSharedString = data;
+    wss.clients.forEach((client) => {
+      client.send(theSharedString);
+    });
+  })
+});
