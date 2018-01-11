726b1413bb62de97467c297e5e48b072545d1df7
Casey Watts <casey.s.watts@gmail.com>
2018-01-11 15:15:43 -0500

    use standard for linting; move stuff to index.html instead of an inline string

--- server.js
+++ server.js
@@ -2,44 +2,28 @@ const express = require('express')
 const ws = require('ws')
 const SocketServer = ws.Server
 const app = express()
+const path = require('path')
 
 // state
-let theSharedString = "hi";
-const PORT = process.env.PORT || 3000;
+let theSharedString = 'hi'
+const PORT = process.env.PORT || 3000
+const INDEX = path.join(__dirname, 'index.html')
 
-const server = app.get('/', (req, res) => res.send(`
-<div>This input field is updated in realtime. Any edit you make will propogate to the other computers looking at this, immediately.</div>
-<input id="theSharedString" value="${theSharedString}">
-<script>
-var HOST = location.origin.replace(/^http/, 'ws')
-var ws = new WebSocket(HOST);
-var el = document.getElementById('theSharedString');
-
-ws.onmessage = function (event) {
-  console.log("received: " + event.data);
-  el.value = event.data;
-};
-
-el.oninput = function (event) {
-  console.log("input: " + el.value);
-  ws.send(el.value);
-}
-</script>
-  `))
+const server = app.get('/', (req, res) => res.sendFile(INDEX))
 .listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
 
-const wss = new SocketServer({ server });
+const wss = new SocketServer({ server })
 
 wss.on('connection', (ws) => {
-  console.log('Client connected');
-  ws.on('close', () => console.log('Client disconnected'));
+  console.log('Client connected')
+  ws.on('close', () => console.log('Client disconnected'))
 
   ws.on('message', (data) => {
-    console.log('received ' + data);
-    theSharedString = data;
+    console.log('received ' + data)
+    theSharedString = data
     wss.clients.forEach((client) => {
       console.log('sending ' + theSharedString)
-      client.send(theSharedString);
-    });
+      client.send(theSharedString)
+    })
   })
-});
+})
